da700397df93e5d0d33d65ff79adbfd528a154a1
laboneeaktar <labonipub@gmail.com>
2022-10-12 06:56:27 +0600

    barchart add

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,14 +1,5 @@
 import React, { useContext } from "react";
 
-// import {
-//   AreaChart,
-//   Area,
-//   XAxis,
-//   YAxis,
-//   CartesianGrid,
-//   Tooltip,
-//
-// } from "recharts";
 import {
   BarChart,
   Bar,
@@ -17,37 +8,38 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
+  ResponsiveContainer,
 } from "recharts";
 import { QuizTopicContext } from "../../layout/Main";
 
 const Statistics = () => {
   const loaderData = useContext(QuizTopicContext);
   const data = loaderData.data;
-  // const { name, total } = data;
-  // console.log(data);
 
   return (
     <div className="mx-auto">
       <h1 className="text-2xl m-10">Total Number of Questions BarChart</h1>
 
-      <BarChart
-        width={500}
-        height={300}
-        data={data}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="total" fill="#8884d8" />
-      </BarChart>
+      <ResponsiveContainer width="100%" height={500}>
+        <BarChart
+          width={50}
+          height={300}
+          data={data}
+          margin={{
+            top: 15,
+            right: 100,
+            left: 100,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="total" fill="#8884d8" />
+        </BarChart>
+      </ResponsiveContainer>
     </div>
   );
 };
